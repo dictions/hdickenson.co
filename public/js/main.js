4bbfb764c167b6f7a857f56ad3fa439b0f2020ed
Ian Williams <iwillinc@gmail.com>
2014-03-13 17:05:43 -0400

    Doozie - set up template js - made letter spacing a global variable (since it is so important to the project) - homepage margin updates

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,45 @@
 'use strict';
-var container = document.querySelector('#portfolio');
-var msnry = new Masonry(container, {
-	'gutter': 16
+
+// sitewide scripts
+var Sitewide = {
+
+	init: function(){
+
+	}
+
+};
+
+// home page scripts
+var HomeTemplate = function(){
+
+	this.masonryContainer = document.querySelector('#portfolio');
+
+};
+
+HomeTemplate.prototype.init = function(){
+
+	var self = this;
+
+	// set up masonry 
+	var msnry = new Masonry(self.masonryContainer, {
+		'gutter': 16,
+		'transitionDuration': '0.25s'
+	});
+
+};
+
+
+// init sitewide and template
+$(window).ready(function(){
+
+	var templateName = $('[data-template]').data('template');
+	var template;
+
+	if(templateName === 'home'){
+		template = new HomeTemplate();
+	}
+
+	Sitewide.init();
+	template.init();
+
 });
